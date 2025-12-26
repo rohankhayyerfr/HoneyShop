5ed754275b7c05c88a35aa8aef42fba1a6daf86d
Rohan <blckreward02@gmail.com>
2025-12-26 13:53:11 +0200

    update part 1

--- HoneyShop/settings.py
+++ HoneyShop/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
