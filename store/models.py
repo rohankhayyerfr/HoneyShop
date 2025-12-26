25dd805476d1ee5acbd42e7c34f1937901e9f767
Rohan <blckreward02@gmail.com>
2025-12-26 14:08:28 +0200

    update part 3

--- store/models.py
+++ store/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
 
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/main/')
     is_main = models.BooleanField(default=False)
     order = models.PositiveSmallIntegerField(default=0)
 
